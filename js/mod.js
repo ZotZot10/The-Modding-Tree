473261469175182c8d2cfeabda5cb7a50f8f30f4
whythenameiuse <otto.zieger10@gmail.com>
2024-05-23 20:15:09 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -46,7 +46,7 @@ function getPointGen() {
 		return new Decimal(0)
 	
 	let gain = new Decimal(1)
-	if (hasUpgrade('p', 11)) gain = gain.times(2)
+	if (hasUpgrade('CMD', 11)) gain = gain.times(2)
 	return gain
 }
 
