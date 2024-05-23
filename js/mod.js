83644ae4acd728cf932d2f053f1bbe04ecf7a81d
whythenameiuse <otto.zieger10@gmail.com>
2024-05-23 18:46:41 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Insperation Of Many Trees",
+	id: "ujf9sd832jjisdfu99uqhfdopdwsd73hdfiosb",
+	author: "ZotZot10",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
