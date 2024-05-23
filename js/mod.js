c09189d8ebad0242d606cadf04dd52c211c2141e
whythenameiuse <otto.zieger10@gmail.com>
2024-05-23 20:01:40 +0200

    try 2
    upgrade 1

--- js/mod.js
+++ js/mod.js
@@ -44,7 +44,7 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-	
+	if (hasUpgrade('CUM', 11)) gain = gain.times(2)
 	let gain = new Decimal(1)
 	return gain
 }
