f182a5ecdcd2000906059c73722207846f304b91
whythenameiuse <otto.zieger10@gmail.com>
2024-05-23 20:00:23 +0200

    reversal to before upgrade 1 effect

--- js/mod.js
+++ js/mod.js
@@ -44,7 +44,7 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-	if (hasUpgrade('C', 11)) gain = gain.times(2)
+	
 	let gain = new Decimal(1)
 	return gain
 }
