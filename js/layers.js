77ff1c63343d4e2da89c58d1b721bc234e5c9f73
whythenameiuse <otto.zieger10@gmail.com>
2024-05-24 10:53:48 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -60,35 +60,3 @@ addLayer("CUM", {
 
 },
 })
-addLayer("Matter", {
-    startData() { return {                  // startData is a function that returns default data for a layer. 
-        unlocked: true,                     // You can add more variables here to add them to your layer.
-        CUM: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
-    }},
-
-    color: "Blue",                       // The color for this layer, which affects many elements.
-    resource: "Matter",            // The name of this layer's main prestige resource.
-    row: 1,                                 // The row this layer is on (0 is the first row).
-
-    baseResource: "Condensed Unknown Matter",                 // The name of the resource your prestige gain is based on.
-    baseAmount() { return player.CUM},  // A function to return the current amount of baseResource.
-
-    requires: new Decimal(10000),              // The amount of the base needed to  gain 1 of the prestige currency.
-                                            // Also the amount required to unlock the layer.
-
-    type: "normal",                         // Determines the formula used for calculating prestige currency.
-    exponent: 0.7,                          // "normal" prestige gain is (currency^exponent).
-
-    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
-        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
-    },
-    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
-        return new Decimal(1)
-    },
-
-    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
-
-    upgrades: {
-        
-    },
-})
